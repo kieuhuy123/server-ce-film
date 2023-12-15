446cd6303051fa57cd135349c62e9af033b2c333
kieuhuy <huypk.codetot@gmail.com>
2023-12-15 18:49:55 +0700

    console.log connectDB

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI, { maxPoolSize: 10 })
+    console.log('connectDB')
   } catch (error) {
     console.log(error)
   }
