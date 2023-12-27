06c87020fb5bda1fa6e365e0f14a6f38ba88e172
kieuhuy <huypk.codetot@gmail.com>
2023-12-27 18:16:10 +0700

    Add feature check auth

---
+++ models/apiKey.model.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const apiKeySchema = new Schema(
+  {
+    key: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    status: {
+      type: Boolean,
+      default: true
+    },
+    permissions: {
+      type: [String],
+      require: true,
+      enum: ['0000', '1111', '2222']
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = mongoose.model('ApiKey', apiKeySchema)
