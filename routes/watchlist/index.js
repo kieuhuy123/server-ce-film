2fff30b8969783ec067244b7093f13bbb088cfc3
kieuhuy <huypk.codetot@gmail.com>
2024-04-16 00:50:19 +0700

    add authenication in route

--- routes/watchlist/index.js
+++ routes/watchlist/index.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const { asyncHandler } = require('../../helpers/asyncHandler')
 const watchlistController = require('../../controllers/watchlist.controller')
+const { authenticationV2 } = require('../../middleware/checkAuth')
 const router = express.Router()
 
+router.use(authenticationV2)
 router
   .route('/')
   .get(asyncHandler(watchlistController.getWatchlist))
