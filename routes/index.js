2044ee431953fbf1a763e380b16c05035a6b50e3
kieuhuy <huypk.codetot@gmail.com>
2024-05-12 15:32:43 +0700

    Update router

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,10 @@ router.use('/v1/api/watchlistMovie', require('./watchlist'))
 router.use('/v1/api/ratingMovie', require('./rating'))
 router.use('/v1/api/movie', require('./movie'))
 router.use('/v1/api/comment', require('./comment'))
+router.use('/v1/api/notification', require('./notification'))
+router.use('/v1/api/discount', require('./discount'))
+router.use('/v1/api/package', require('./package'))
+router.use('/v1/api/order', require('./order'))
 router.use('/v1/api', require('./access'))
 
 module.exports = router
